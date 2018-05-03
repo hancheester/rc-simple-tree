faa3a63d6caf26e8626e0b13a20e6c2c21fba352
Han Chee Tan <hancheester@gmail.com>
2018-05-03 16:45:05 +0100

    Renamed some files

--- src/DropPositions.js
+++
@@ -1,3 +0,0 @@
-export const DROP_HINT_TOP = -1;
-export const DROP_HINT_BORDER = 0;
-export const DROP_HINT_BOTTOM = 1;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Tree from './Tree';
-import TreeNode from './TreeNode';
+import Tree from './tree';
+import TreeNode from './tree-node';
 
 export { Tree, TreeNode };
